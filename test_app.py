069955f9dcbe02e86d518e4abaa55eb31415a567
Anastasia Tinkaluk <150711836+anaqllx@users.noreply.github.com>
2025-11-29 19:27:26 +0200

    Fix formatting in test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,11 @@
 import pytest
-from app import add, subtract 
+from app import add, subtract
+
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
 
 def test_subtract():
     assert subtract(5, 2) == 3
